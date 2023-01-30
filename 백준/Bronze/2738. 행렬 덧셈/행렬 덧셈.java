e01f1a9a8dffbc992765f73f222d2f54a586caff
ksr0818 <120084774+ksr0818@users.noreply.github.com>
2023-01-30 20:23:22 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 612 ms, Memory: 36008 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] matrix1 = new int[N][M];
+        int[][] matrix2 = new int[N][M];
+        int[][] matrix3 = new int[N][M];
+
+
+        for (int i =0; i <N; i++) {
+            for (int j=0; j < M; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i =0; i <N; i++) {
+            for (int j=0; j < M; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i =0; i <N; i++) {
+            for (int j=0; j < M; j++) {
+                int num = matrix1[i][j] + matrix2[i][j];
+                System.out.println(num);
+            }
+        }
+    }
+}
