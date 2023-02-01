126a739971bb9f000a388f6c6b9cca8f59e7811c
ksr0818 <120084774+ksr0818@users.noreply.github.com>
2023-02-01 22:00:20 +0900

    Update 카드2.java

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -2,21 +2,29 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        //scanner 설정
         Scanner sc = new Scanner(System.in);
+        
+        // 카드 개수를 scanner를 통해 받기
         int input = sc.nextInt();
-
+        
+        //Queue 생성
         Queue<Integer> queue = new LinkedList<>();
-
+        
+        // for를 통해 queue에 카드 생성 1~input까지
         for (int i=1; i<=input; i++) {
             queue.add(i);
         }
-
+        
+        
+        // queue size가 1이 될 때 까지 버리고 맨아래 추가하기를 반복!
         while(queue.size()>1) {
             queue.poll();
             queue.add(queue.peek());
             queue.poll();
         }
-
+        
+        // queue size가 1일 때 그 값을 프린트
         System.out.println(queue.peek());
     }
 }
