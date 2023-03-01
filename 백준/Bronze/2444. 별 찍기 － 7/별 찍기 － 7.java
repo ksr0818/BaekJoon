aaca5f35bca114c3405dfed37eabb8503c6c4415
ksr0818 <120084774+ksr0818@users.noreply.github.com>
2023-03-01 13:08:39 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 224 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+
+        for (int i =1; i<= num; i++) {
+            System.out.println(" ".repeat(num-i) + "*".repeat(2*i-1));
+        }
+
+        for (int i=num-1; i >=1; i--) {
+            System.out.println(" ".repeat(num-i) + "*".repeat(2*i-1));
+        }
+
+    }
+}
+
