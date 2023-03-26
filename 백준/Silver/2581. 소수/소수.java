b30e4379f931d9f39b7fdb154dfca766947614db
ksr0818 <120084774+ksr0818@users.noreply.github.com>
2023-03-26 09:25:13 +0900

    [Silver V] Title: 소수, Time: 260 ms, Memory: 18412 KB -BaekjoonHub

---
+++ 백준/Silver/2581. 소수/소수.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        int sum = 0;
+        int min = N;
+        x : for (int i = M; i <= N; i++) { 
+            if (i == 1) {
+                continue;
+            }
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    continue x;
+                }
+            }
+            sum += i;
+            if (min > i) {
+                min = i;
+            }
+        }
+        if (sum == 0 ) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(sum + "\n" + min);
+
+    }
+
+}
