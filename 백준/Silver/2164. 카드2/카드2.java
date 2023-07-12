eafb1508c2e49d76b713aad8f89b0c7450ea7e2a
ksr0818 <ksr940818@gmail.com>
2023-07-13 02:34:38 +0900

    [Silver IV] Title: 카드2, Time: 272 ms, Memory: 48996 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,30 +1,20 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //scanner 설정
         Scanner sc = new Scanner(System.in);
-        
-        // 카드 개수를 scanner를 통해 받기
-        int input = sc.nextInt();
-        
-        //Queue 생성
         Queue<Integer> queue = new LinkedList<>();
-        
-        // for를 통해 queue에 카드 생성 1~input까지
-        for (int i=1; i<=input; i++) {
+        int N = sc.nextInt();
+        for (int i =1; i <= N; i++) {
             queue.add(i);
         }
-        
-        
-        // queue size가 1이 될 때 까지 버리고 맨아래 추가하기를 반복!
-        while(queue.size()>1) {
+
+        while(queue.size() > 1) {
             queue.poll();
-            queue.add(queue.peek());
-            queue.poll();
+            queue.add(queue.poll());
         }
-        
-        // queue size가 1일 때 그 값을 프린트
-        System.out.println(queue.peek());
+        System.out.println(queue.poll());
     }
 }
