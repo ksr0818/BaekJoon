1ac7d8c8f18a5b3e598457c331a1c6fa44926fba
ksr0818 <ksr940818@gmail.com>
2023-07-20 11:23:08 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2332 ms, Memory: 363652 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
