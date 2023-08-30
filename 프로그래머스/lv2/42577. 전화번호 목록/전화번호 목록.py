18669c1f6c5a607416142e95e147b7aa9965c91f
ksr0818 <ksr940818@gmail.com>
2023-08-30 20:04:36 +0900

    [level 2] Title: 전화번호 목록, Time: 178.38 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,19 @@
+def solution(phone_book):
+    # phone_book을 순회하면서 전체 전화번호를 키로 가지는 딕셔너리 생성
+    table = {}
+        # value는 True로 설정
+    for num in phone_book:
+        table[num] = True
+        
+    # phone_book을 다시 순회하며, 각각의 전화번호를 가지고
+    for number in phone_book:
+        str = ""
+        # 문자열을 맨 앞부터 잘라서 더해가며, 해당 문자열을 키로 가지는 값이 테이블에 있다면
+        for char in number[:-1]:
+            str += char
+            # 접두어가 존재하는 것으로, False 반환
+            if str in table:
+                return False
+            
+    # 순회가 모두 종료될때까지  False를 반환하지 않는다면, 접두어가 없으므로 True를 반환
+    return True
