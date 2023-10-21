65d3b3577226fac43494fb776b308b5219c79f52
ksr0818 <ksr940818@gmail.com>
2023-10-21 23:07:51 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int num = x;
+        
+        while(num>=10) {
+            sum += num%10;
+            num /= 10;
+        }
+        sum += num%10;
+        
+        if (x % sum !=0) answer = false; 
+        
+        return answer;
+    }
+}
