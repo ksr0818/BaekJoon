296f43ba55133ebd2690f95b512b677d0023b1ab
ksr0818 <ksr940818@gmail.com>
2024-01-04 14:46:08 +0900

    [level 2] Title: 모음 사전, Time: 3.05 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,26 @@
+import java.util.*; 
+class Solution {
+    static List<String> list = new ArrayList<>();
+    static String [] words = {"A", "E", "I", "O", "U"};
+    public int solution(String word) {
+        int answer = 0;
+        
+        dfs("", 0);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(word)) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    static void dfs(String str, int len) {
+        list.add(str);
+        if (len == 5) return;
+        for (int i = 0; i < 5; i++) {
+            dfs(str + words[i], len + 1);
+        }
+    }
+}
