2138e01d2c7842162899896fbe772b0531e7c9d0
ksr0818 <ksr940818@gmail.com>
2024-01-05 23:21:55 +0900

    [level 2] Title: 소수 찾기, Time: 19.68 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 23:21:24
+2024년 1월 5일 23:21:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,28 +1,18 @@
-import java.util.*;
-
 class Solution {
-    
-    public static HashSet<Integer> numberSet = new HashSet<>();
-    
-    public int solution(String numbers) {
-        int count = 0;
+    public int solution(int n) {
+    	// 2는 소수이기 때문에 초기값 1로 선언
+        int answer = 1;
         
-        recursive("", numbers);
-
-        
-        for (int num : numberSet) {
-            if(isPrime(num)) {
-                count ++;
-            }
+        // 3부터 n까지 반복하여 소수의 개수 찾기
+        for (int i = 3; i <= n; i++) {
+            if (isPrime(i)) answer += 1;
         }
         
-        return count;
+        return answer;
     }
     
-    private static boolean isPrime(int num) {
-        if(num == 1 || num == 0) {
-            return false;
-        }
+    // 소수 판별 메서드
+    private boolean isPrime(int num) {
         for (int i = 2; i <= (int) Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
@@ -31,23 +21,4 @@ class Solution {
         
         return true;
     }
-    
-    // 입력 받은 숫자를 나올 수 있는 최대의 조합을 구해서 HashSet으로 만들어 반환한다.
-        private void recursive(String comb, String others) {
-            // 현재 조합을 set에 추가한다.
-            if(!comb.equals("")) {
-                numberSet.add(Integer.parseInt(comb));
-            }
-    
-            // 남은 숫자 중 한개를 더해 새로운 조합을 만든다.
-            for(int i=0; i<others.length(); i++) {
-                if(!comb.equals("")) {
-                    // HashSet의 타입을 Integer형으로 만들어준다.
-                    numberSet.add(Integer.valueOf(comb));
-                }
-    
-                recursive(comb + others.charAt(i), others.substring(0, i) + others.substring(i+1));
-            }
-        }
-    
 }
